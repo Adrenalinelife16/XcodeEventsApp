df117164ba2d37e092ece7c2c7db6cb9d213db98
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-02-06 16:27:16 -0600

    Updated Logout group with header and main files

---
+++ Events/LogoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogoutViewController.h
+//  Events
+//
+//  Created by Michael Cather on 2/6/17.
+//  Copyright © 2017 Teknowledge Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogoutViewController : UIViewController
+
+@end
