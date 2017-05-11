16b5c0abd8f11b5fd2bbb6fe8f96bf27c2bed1ec
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-05-11 18:00:58 -0500

    all scroll views working

---
+++ Events/More/PolicyViewController.m
@@ -0,0 +1,39 @@
+//
+//  PolicyViewController.m
+//  Events
+//
+//  Created by Michael Cather on 5/11/17.
+//  Copyright © 2017 Teknowledge Software. All rights reserved.
+//
+
+#import "PolicyViewController.h"
+
+@interface PolicyViewController ()
+
+@end
+
+@implementation PolicyViewController
+@synthesize scrollView;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationController.navigationBar.topItem.title = @"";
+    [self.navigationController.navigationBar setTintColor:[UIColor redColor]];
+    
+    policyTextView.text = NSLocalizedString(@"CONTENT_PRIVACY", "CONTENT_PRIVACY");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    [self.scrollView setContentSize:CGSizeMake(200, 2000)];
+}
+
+
+@end
