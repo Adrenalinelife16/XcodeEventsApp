6b5ca0f78a78e1305c2678a0903b6543cc5bccbc
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-06-26 21:38:24 -0500

    Favorites displaying

---
+++ Events/MyFavoriteCustomCell.h
@@ -0,0 +1,19 @@
+//
+//  MyFavoriteCustomCell.h
+//  Events
+//
+//  Created by Michael Cather on 6/26/17.
+//  Copyright © 2017 Teknowledge Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyFavoriteCustomCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UIImageView *images;
+@property (strong, nonatomic) IBOutlet UILabel *lblDateTime;
+@property (strong, nonatomic) IBOutlet UILabel *lblEventName;
+@property (strong, nonatomic) IBOutlet UILabel *lblEventDesc;
+@property (strong, nonatomic) IBOutlet UIImageView *imgEventImage;
+
+@end
