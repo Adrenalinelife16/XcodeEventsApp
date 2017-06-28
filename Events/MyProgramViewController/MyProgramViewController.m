8dd37576efcc5d412120ca29d8ec981c00f8e4e8
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-06-27 21:48:00 -0500

    working segue

--- Events/MyProgramViewController/MyProgramViewController.m
+++ Events/MyProgramViewController/MyProgramViewController.m
@@ -76,6 +76,7 @@
     self.imgSegmentBar.image=[UIImage imageNamed:@"Segmented_left.png"];
     
     [self clickedMyCalender:nil];
+    [self getAllEventsFromServer];
 
     
     
@@ -469,10 +470,96 @@
     NSLog(@"final Array = %@", finalArray);
 }
 
+-(void)getAllEventsFromServer {
+    
+    
+    NSDictionary *dictOfEventRequestParameter = [[NSDictionary alloc] initWithObjectsAndKeys:[Utility getNSUserDefaultValue:KUSERID],@"user_id", nil];
+    
+    [Utility GetDataForMethod:NSLocalizedString(@"GETEVENTS_METHOD", @"GETEVENTS_METHOD") parameters:dictOfEventRequestParameter key:@"" withCompletion:^(id response){
+        
+        [DSBezelActivityView removeViewAnimated:NO];
+        arrayFavouriteProgram  =   [[NSMutableArray alloc] init];
+        
+        
+        if ([response isKindOfClass:[NSArray class]]) {
+            if ([[[response objectAtIndex:0] allKeys] containsObject:@"status"]) {
+                if ([[[response objectAtIndex:0] objectForKey:@"status"] intValue] == 0) {
+                    [Utility alertNotice:@"" withMSG:[[response objectAtIndex:0] objectForKey:@"message"] cancleButtonTitle:@"OK" otherButtonTitle:nil];
+                    gArrayEvents = [[NSMutableArray alloc] initWithArray:arrayFavouriteProgram];
+                    //                 [self.tblMainTable reloadData];
+                    return;
+                }
+            }
+            
+            NSSortDescriptor *descriptor=[[NSSortDescriptor alloc] initWithKey:@"event_start_date"  ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+            NSArray *sortedArrayEventList = [response sortedArrayUsingDescriptors:@[descriptor]];
+            
+            
+            for (NSDictionary *dict in sortedArrayEventList) {
+                EventList *eventObj = [[EventList alloc] init];
+                eventObj.eventID                =   [NSNumber numberWithInt:[[dict objectForKey:@"event_id"] intValue]];
+                eventObj.eventName              =   [dict objectForKey:@"event_name"];
+                eventObj.eventImageURL          =   [dict objectForKey:@"event_image_url"];
+                //       NSLog(@"Data%@",sortedArrayEventList);
+                eventObj.eventDescription       =   [dict objectForKey:@"event_content"];
+                
+                //12.15pm 4 June '14
+                eventObj.eventStartDateTime     =   [Utility getFormatedDateString:[NSString stringWithFormat:@"%@ %@",[dict objectForKey:@"event_start_date"],[dict objectForKey:@"event_start_time"]] dateFormatString:@"yyyy-MM-dd HH:mm:ss" dateFormatterString:@"E, MMM d yyyy h:mm a"];
+                
+                eventObj.eventEndDateTime       =   [Utility getFormatedDateString:[NSString stringWithFormat:@"%@ %@",[dict objectForKey:@"event_end_date"],[dict objectForKey:@"event_end_time"]] dateFormatString:@"yyyy-MM-dd HH:mm:ss" dateFormatterString:@"E, MMM d yyyy h:mm a"];
+                
+                eventObj.eventLocationName      =   [dict objectForKey:@"location_name"];
+                eventObj.eventLocationAddress   =   [dict objectForKey:@"location_address"];
+                eventObj.eventLocationTown      =   [dict objectForKey:@"location_town"];
+                eventObj.eventLocationpostcode  =   [dict objectForKey:@"location_postcode"];
+                eventObj.eventLocationState     =   [dict objectForKey:@"location_state"];
+                eventObj.eventLocationCountry   =   [dict objectForKey:@"location_country"];
+                eventObj.eventLocationLatitude  =   [NSNumber numberWithFloat:[[dict objectForKey:@"location_latitude"] floatValue]];
+                eventObj.eventLocationLongitude =   [NSNumber numberWithFloat:[[dict objectForKey:@"location_longitude"] floatValue]];
+                
+                
+                
+                if ([[dict objectForKey:@"ticket"] count]>0) {
+                    NSDictionary *dictOfTicket  =   [[NSDictionary alloc] initWithDictionary:[dict objectForKey:@"ticket"]];
+                    eventObj.eventTicketName            =   [dictOfTicket objectForKey:@"ticket_name"];
+                    eventObj.eventTicketDescription     =   [dictOfTicket objectForKey:@"ticket_description"];
+                    eventObj.eventTicketPrice           =   [dictOfTicket objectForKey:@"ticket_price"];
+                    eventObj.eventTicketStart           =   [dictOfTicket objectForKey:@"ticket_start"];
+                    eventObj.eventTicketEnd             =   [dictOfTicket objectForKey:@"ticket_end"];
+                    eventObj.eventTicketMembers         =   [dictOfTicket objectForKey:@"ticket_members_roles"];
+                    eventObj.eventTicketGuests          =   [dictOfTicket objectForKey:@"ticket_guests"];
+                    eventObj.eventTicketRequired        =   [dictOfTicket objectForKey:@"ticket_required"];
+                    eventObj.eventTicketAvailSpaces     =   [NSNumber numberWithInt:[[dictOfTicket objectForKey:@"avail_spaces"] intValue]];
+                    eventObj.eventTicketBookedSpaces    =   [NSNumber numberWithInt:[[dictOfTicket objectForKey:@"booked_spaces"] intValue]];
+                    eventObj.eventTicketTotalSpaces     =   [NSNumber numberWithInt:[[dictOfTicket objectForKey:@"total_spaces"] intValue]];
+                }
+                
+                
+                [arrayFavouriteProgram addObject:eventObj];
+                
+                
+            }
+        }
+        
+        /**
+         *  global array for events data.
+         */
+        gArrayEvents = [[NSMutableArray alloc] initWithArray:arrayFavouriteProgram];
+        
+    }WithFailure:^(NSString *error)
+     {
+         [DSBezelActivityView removeViewAnimated:NO];
+         
+         
+     }];
+    
+}
+
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     
- if ([segue.identifier isEqualToString:@"event_name"]) {
+ if ([segue.identifier isEqualToString:@"event_cal"]) {
         
         NSIndexPath *selectedRowIndex = [self.tblMainTable indexPathForSelectedRow];
         AboutViewController *aboutVwController = [segue destinationViewController];
