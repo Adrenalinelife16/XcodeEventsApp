dc484194dd1037d6becafe4de8dcc96cb5e08214
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-07-26 08:44:19 -0500

    push for Back up

--- Events/ReviewCreateEventViewController.h
+++ Events/ReviewCreateEventViewController.h
@@ -10,4 +10,26 @@
 
 @interface ReviewCreateEventViewController : UIViewController
 
+// Fields for event submit
+@property (strong, nonatomic) IBOutlet UILabel *eventName;
+@property (strong, nonatomic) IBOutlet UILabel *category;
+@property (strong, nonatomic) IBOutlet UILabel *locationName;
+@property (strong, nonatomic) IBOutlet UILabel *address;
+@property (strong, nonatomic) IBOutlet UILabel *city;
+@property (strong, nonatomic) IBOutlet UILabel *state;
+@property (strong, nonatomic) IBOutlet UILabel *zipCode;
+@property (strong, nonatomic) IBOutlet UITextView *detailView;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+
+// UI Format
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollViewCE;
+@property (strong, nonatomic) IBOutlet UILabel *startDate;
+@property (strong, nonatomic) IBOutlet UILabel *endDate;
+@property (strong, nonatomic) IBOutlet UILabel *startTime;
+@property (strong, nonatomic) IBOutlet UILabel *endTime;
+@property (strong, nonatomic) IBOutlet UIButton *buttonBorder;
+
+
 @end
--- Events/ReviewCreateEventViewController.m
+++ Events/ReviewCreateEventViewController.m
@@ -24,14 +24,52 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+- (IBAction)submitEvent:(id)sender {
+    
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    
+    NSDictionary *dictOfParameters  =   [[NSDictionary alloc] initWithObjectsAndKeys:self.eventName.text,@"event_name", self.startDate.text,@"start_date",self.endDate.text,@"end_date",self.startTime.text,@"start_time",self.endTime.text,@"end_time", self.locationName.text,@"location_name",self.address.text,@"location_address",self.city.text,@"location_city",self.state.text,@"location_state",self.zipCode.text,@"location_zip",self.detailView.text,@"event_info",self.imageView.image,@"image",self.category.text,@"category", nil];
+    
+    [Utility GetDataForMethod:NSLocalizedString(@"CREATE_EVENT_METHOD", @"CREATE_EVENT_METHOD") parameters:dictOfParameters key:@"" withCompletion:^(id response){
+        
+        
+        if ([response isKindOfClass:[NSDictionary class]]) {
+            if ([[response objectForKey:@"message"] isEqualToString:@"Sorry, that username already exists!"]) {
+                [Utility alertNotice:APPNAME withMSG:[response objectForKey:@"message"] cancleButtonTitle:@"OK" otherButtonTitle:nil];
+                
+                
+            }
+            
+            
+            else{
+                
+                
+            }
+            
+        }
+        
+        else if ([response isKindOfClass:[NSArray class]]) {
+            
+            if ([[[response objectAtIndex:0] objectForKey:@"message"] isEqualToString:@"Sorry, that email address is already used!"]) {
+                [Utility alertNotice:APPNAME withMSG:[[response objectAtIndex:0] objectForKey:@"message"] cancleButtonTitle:@"OK" otherButtonTitle:nil];
+                
+                
+            }
+            else{
+                
+                
+            }
+            
+            
+        }
+        [DSBezelActivityView removeViewAnimated:YES];
+        
+    }
+                  WithFailure:^(NSString *error){
+                      [DSBezelActivityView removeViewAnimated:YES];
+                      NSLog(@"%@",error);
+                  }];
+    
 }
-*/
 
 @end
