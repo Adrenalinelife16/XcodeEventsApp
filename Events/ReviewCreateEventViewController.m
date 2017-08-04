09d8a8c38b26fa49e6a10558b7c5b2c10cb88aac
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-08-04 15:31:16 -0500

    create event working

--- Events/ReviewCreateEventViewController.m
+++ Events/ReviewCreateEventViewController.m
@@ -207,31 +207,10 @@
     
     if ([string isEqualToString:@"Ok"]) {
         
-        ProgramViewController *viewController = [[ProgramViewController alloc] init];
-        
-        [self topViewControllerWithRootViewController:viewController];
-        
+        [self.navigationController popToRootViewControllerAnimated:YES];
     }
     
 }
 
-- (UIViewController*)topViewController {
-    return [self topViewControllerWithRootViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
-}
-
-- (UIViewController*)topViewControllerWithRootViewController:(UIViewController*)rootViewController {
-    if ([rootViewController isKindOfClass:[UITabBarController class]]) {
-        UITabBarController* tabBarController = (UITabBarController*)rootViewController;
-        return [self topViewControllerWithRootViewController:tabBarController.selectedViewController];
-    } else if ([rootViewController isKindOfClass:[UINavigationController class]]) {
-        UINavigationController* navigationController = (UINavigationController*)rootViewController;
-        return [self topViewControllerWithRootViewController:navigationController.visibleViewController];
-    } else if (rootViewController.presentedViewController) {
-        UIViewController* presentedViewController = rootViewController.presentedViewController;
-        return [self topViewControllerWithRootViewController:presentedViewController];
-    } else {
-        return rootViewController;
-    }
-}
 
 @end
