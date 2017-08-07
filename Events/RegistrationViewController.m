45ed0c938651105feef64467546b731000e345a5
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-08-07 15:50:02 -0500

    commit before register email fix

--- Events/RegistrationViewController.m
+++ Events/RegistrationViewController.m
@@ -109,6 +109,12 @@
         NSString *FirstLast = [NSString stringWithFormat:@" %@ %@", Fname, Lname];
         NSString *noCaps = [self.txtUsername.text lowercaseString];
         
+        
+        NSString *string1 = @"Sorry, that username already exists!";
+        NSString *string2 = @"Sorry, that email address is already used!";
+
+        
+        
         NSDictionary *dictOfParameters  =   [[NSDictionary alloc] initWithObjectsAndKeys:FirstLast,@"name", self.txtUsername.text,@"user_login",noCaps,@"login_name", self.txtEmail.text,@"user_email",self.txtPassword.text,@"pwd", nil];
         
         [Utility GetDataForMethod:NSLocalizedString(@"REGISTER_METHOD", @"REGISTER_METHOD") parameters:dictOfParameters key:@"" withCompletion:^(id response){
@@ -118,8 +124,7 @@
                     [Utility alertNotice:APPNAME withMSG:[response objectForKey:@"message"] cancleButtonTitle:@"OK" otherButtonTitle:nil];
                     
     
-                     NSLog(@"username already exist");
-                
+                    
                 }
                 
                 
@@ -151,8 +156,8 @@
                     UIAlertView *av = [[UIAlertView alloc] initWithTitle:APPNAME message:[[response objectAtIndex:0] objectForKey:@"message"] delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
                     [av show];
                     
-              //      [self.tabBarController setSelectedIndex:0];
-              //      [self.navigationController popViewControllerAnimated:NO];
+                    [self.tabBarController setSelectedIndex:0];
+                    [self.navigationController popViewControllerAnimated:NO];
                 
                     
                 }
