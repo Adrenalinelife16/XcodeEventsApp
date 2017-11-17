ca7b73534c1322a302afefa5f3e00c038e4760df
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-11-17 12:24:04 -0600

    working on remote notifications

---
+++ OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Michael Cather on 11/17/17.
+//  Copyright © 2017 Adrenaline Life. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
