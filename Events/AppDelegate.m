5d5291871de87a9a06afe1e8d0f568cc31df944a
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-11-19 17:21:23 -0600

    removing all the bullshit

--- Events/AppDelegate.m
+++ Events/AppDelegate.m
@@ -10,7 +10,6 @@
 #import "ProgramViewController.h"
 #import <Fabric/Fabric.h>
 #import <Crashlytics/Crashlytics.h>
-#import <OneSignal/OneSignal.h>
 
 
 @implementation AppDelegate 
@@ -44,26 +43,6 @@
     
     [Fabric with:@[[Crashlytics class]]];
     
-    // Replace '11111111-2222-3333-4444-0123456789ab' with your OneSignal App ID.
-    [OneSignal initWithLaunchOptions:launchOptions
-                               appId:@"7893c05e-649b-402a-8f04-ad41cbeb0235"
-            handleNotificationAction:nil
-                            settings:@{kOSSettingsKeyAutoPrompt: @false}];
-    OneSignal.inFocusDisplayType = OSNotificationDisplayTypeNotification;
-    
-    // Recommend moving the below line to prompt for push after informing the user about
-    //   how your app will use them.
-    [OneSignal promptForPushNotificationsWithUserResponse:^(BOOL accepted) {
-        NSLog(@"User accepted notifications: %d", accepted);
-    }];
-    
-    // Call syncHashedEmail anywhere in your iOS app if you have the user's email.
-    // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-    // [OneSignal syncHashedEmail:userEmail];
-    
-
-
-
     return YES;
 }
 
