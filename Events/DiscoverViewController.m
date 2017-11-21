c2db376017d69266e5e4eb6cb83e299e5c2f0e14
Michael Cather Cather <mgcather@crimson.ua.edu>
2017-11-20 21:15:28 -0600

    discover page working on scroll again

--- Events/DiscoverViewController.m
+++ Events/DiscoverViewController.m
@@ -60,7 +60,7 @@
 - (void)viewDidLayoutSubviews
 {
     [super viewDidLayoutSubviews];
-    [self.scrlVW setContentSize:CGSizeMake(100, 1835)]; //Change height here when adding more categories to discover page
+    [self.scrlVW setContentSize:CGSizeMake(100, 1950)]; //Change height here when adding more categories to discover page
 }
 
 
